a332dd0823188cc13c3447b07510564ec0d992a6
Ali <ali@tomorrowagency.com>
2023-08-21 18:25:21 +0100

    init-commit

---
+++ 1_ruby.rb
@@ -0,0 +1,59 @@
+# 
+#
+#
+# ######### arithmetic #########
+#
+#
+#
+puts 1 + 1
+# 2
+
+puts 9_999_999 
+# 9999999
+
+puts 1_000
+# 1000
+
+=begin
+  this
+  is
+  a
+  multi-line
+  comment
+  but don't use it!
+=end
+
+# Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+puts 2 + 3 * 5
+# 17
+puts (2 + 3) * 5
+# 25
+puts 10 / 5
+# 2
+puts 12 / 5
+# 2
+puts 12.0 / 5
+# 2.4
+puts 12 / 5.0
+# 2.4
+puts 12.0 / 5.0
+# 2.4
+puts 0.5 / 2
+# 0.25
+puts 5 % 2
+# 1
+puts 7 % 2
+# 1
+puts 6 % 2
+# 0
+
+
+# 
+#
+#
+# ######### concatenation #########
+#
+#
+#
+puts "race" + "car"
+#racecar
